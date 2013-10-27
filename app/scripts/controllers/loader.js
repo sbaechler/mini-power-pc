b20b6045f947e072bc2a48865c7a2c397f785834
miltom <mili92@gmail.com>
2013-10-27 21:27:10 +0100

    startedMultiply

--- app/scripts/controllers/loader.js
+++ app/scripts/controllers/loader.js
@@ -38,33 +38,35 @@ angular.module('miniPowerPCLoader', ['sysconvProvider', 'memoryProvider', 'miniP
             "END"].join("\n")
         },
         {'name': 'Speicher 500 * Speicher 502', 'code': [
-            /*init*/
-            "LWDD 02, #502", /*Hier steht das zwischenergebnis. am anfang ist der wert = 2.zah*/
+            LWDD 00, #504
+            SLA
+            SWDD 00, #504
 
-            /*Schleifenstart.der wert des zähler ist am anfang 0*/
-            "LWDD 00, #504", /*Zähler aus der adresse in akku speichern*/
-            "SLA", /*zähler um eins nach links verschieben*/
-            "SWDD 00, #504", /*zähler zurück in die adresse laden*/
-            "BZD #", /*wenn der akku ==0 ist, sollte abgebrochen werden*/
 
-            "LWDD 01, #504", /*Zähler aus der adresse nach register 1 speichern*/
-            "LWDD 00, #504",/*1. zahl der mult. aus der adresse ins akku speichern*/
-            "AND 01", /*Logisches & mit akku (1 Zahl) und R1 (zähler). schauen ob an der stelle eine 1 oder 0 steht.*/
+            LWDD 00, #506
+            SLA
+            SWDD 00, #506
 
-            /*wenn der akku ==0 ist*/
-            "BZD #", /*wenn der akku 0 ist, wiederhole die schleife*/
+            LWDD 00, #502
+            SLA
+            SWDD 00, #502
+            BCD #Sprung
+            LWDD 00, #504
+            ADDD #500
+            SWDD 00, #504
 
-            /*wenn der akku !=0 ist*/
-            "LWDD 00, #502", /* zweite zahl der multiplikation ins akku laden*/
-            "INC", /*Akkuinhalt um eins erhöhen bzw um ein verschieben*/
-            "SWDD 00, #502", /*die verschobene 2. zahl zurück in die adresse speichern*/
-            "ADD 02", /*zwischenergebnis + verschobene 2. zahl, ergebnis steht in der akku*/
-            "LWDD 03, #502", /*die verschobene 2. zahl ins register 3 verschieben*/
-            "SWDD 00, #502", /*das neue ergebnis in die adresse speichern*/
-            "LWDD 02, #502", /*das neue ergebnis ins register 2 verschieben*/
-            "SWDD 03, #502", /*die 2. zahl wieder zurück in die adresse speichern*/
-            "BD #", /*unbedingter sprung, wiederhole die schleife*/
-            "END"].join("\n")
+            BCD [#Sprung]
+            LWDD 00, #506
+            INC
+            SWDD 00, #506
+            BD NOADD
+
+            LWDD 00, #508
+            DEC
+
+      BNZD #Sprung
+
+      "END"].join("\n")
         }
     ];
     $scope.fillProgram = function(value){
