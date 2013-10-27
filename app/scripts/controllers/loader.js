eb6d2b5daf0af686f9344f30a6bbf50cf997380f
miltom <mili92@gmail.com>
2013-10-27 15:19:53 +0100

    algorithmus

--- app/scripts/controllers/loader.js
+++ app/scripts/controllers/loader.js
@@ -37,7 +37,35 @@ angular.module('miniPowerPCLoader', ['sysconvProvider', 'memoryProvider', 'miniP
             "SWDD 00 506",
             "END"].join("\n")
         },
-        {'name': 'Speicher 500 * Speicher 502', code: ""}
+        {'name': 'Speicher 500 * Speicher 502', 'code': [
+            /*init*/
+            "LWDD 02, #502", /*Hier steht das zwischenergebnis. am anfang ist der wert = 2.zah*/
+
+            /*Schleifenstart.der wert des zähler ist am anfang 0*/
+            "LWDD 00, #504", /*Zähler aus der adresse in akku speichern*/
+            "SLA", /*zähler um eins nach links verschieben*/
+            "SWDD 00, #504", /*zähler zurück in die adresse laden*/
+            "BZD #", /*wenn der akku ==0 ist, sollte abgebrochen werden*/
+
+            "LWDD 01, #504", /*Zähler aus der adresse nach register 1 speichern*/
+            "LWDD 00, #504",/*1. zahl der mult. aus der adresse ins akku speichern*/
+            "AND 01", /*Logisches & mit akku (1 Zahl) und R1 (zähler). schauen ob an der stelle eine 1 oder 0 steht.*/
+
+            /*wenn der akku ==0 ist*/
+            "BZD #", /*wenn der akku 0 ist, wiederhole die schleife*/
+
+            /*wenn der akku !=0 ist*/
+            "LWDD 00, #502", /* zweite zahl der multiplikation ins akku laden*/
+            "INC", /*Akkuinhalt um eins erhöhen bzw um ein verschieben*/
+            "SWDD 00, #502", /*die verschobene 2. zahl zurück in die adresse speichern*/
+            "ADD 02", /*zwischenergebnis + verschobene 2. zahl, ergebnis steht in der akku*/
+            "LWDD 03, #502", /*die verschobene 2. zahl ins register 3 verschieben*/
+            "SWDD 00, #502", /*das neue ergebnis in die adresse speichern*/
+            "LWDD 02, #502", /*das neue ergebnis ins register 2 verschieben*/
+            "SWDD 03, #502", /*die 2. zahl wieder zurück in die adresse speichern*/
+            "BD #", /*unbedingter sprung, wiederhole die schleife*/
+            "END"].join("\n")
+        }
     ];
     $scope.fillProgram = function(value){
         if (value){
