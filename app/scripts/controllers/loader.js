e45e5518db1d7a22e0a7418957d4bbf11784e623
miltom <mili92@gmail.com>
2013-10-30 23:51:23 +0100

    update multiply adresses

--- app/scripts/controllers/loader.js
+++ app/scripts/controllers/loader.js
@@ -51,87 +51,85 @@ angular.module('miniPowerPCLoader', ['sysconvProvider', 'memoryProvider', 'miniP
       "LWDD 00 500",     // Lade Faktor a in den Akku
       "SLL",             // Shift nach links
       "BCD 112",         // 1. Bit ist 1 -> negative Zahl, muss invertiert werden.
-      "BD 126",
+      "BD 126",          // Adresse 110
       "LWDD 00 500",     // Faktor a neu laden
       "NOT",             // Invertieren
       "INC",             // 1 hinzufügen
       "SWDD 00 500",     // Positive Zahl in Speicher 500 schreiben.
-      "CLR 00",
+      "CLR 00",           //Adresse 120
       "INC",
       "SWDD 00 512",     // 1 in Speicher 512 schreiben.
 
       //check b Adresse 126
       "LWDD 00 502",     // Lade Faktor b in den Akku
       "SLL",             // Shift nach links
-      "BCD 134",         // 1. Bit ist 1 -> negative Zahl, muss invertiert werden.
-      "BD 152",
+      "BCD 134",         // Adresse 130: 1. Bit ist 1 -> negative Zahl, muss invertiert werden.
+      "BD 148",
       "LWDD 00 502",     // Faktor a neu laden
       "NOT",             // Invertieren
       "INC",             // 1 hinzufügen
-      "SWDD 00 502",     // Positive Zahl in Speicher 500 schreiben.
+      "SWDD 00 502",     // Adresse 140: Positive Zahl in Speicher 500 schreiben.
       "LWDD 00 512",     // Wert aus Speicher 512 laden
       "INC",
       "SWDD 00 512",     // 1 in Speicher 512 schreiben.
 
 
-      //load Adresse 152
+      //load Adresse 148
       "LWDD 10 500",     // Lade Faktor a in Register 10
-      "CLR 01",          // Register 01 löschen
+      "CLR 01",          // Adresse 150: Register 01 löschen
       "CLR 00",          // Akku löschen
       "ADDD 16",         // 16 in den Akku schreiben.
       "SWDD 00 510",     // Die Zahl (16) in Speicher #510 schreiben. Dies ist ein Zähler
       "SWDD 10 504",     // Faktor a in Speicher #504 zwischenspeichern.
-      "SWDD 01 506",     // Speicher #506 löschen.
+      "SWDD 01 506",     // Adresse 160: Speicher #506 löschen.
 
       //mult: Adresse 168
       "LWDD 00 504",
       "SLL",             // Low Byte nach links
       "SWDD 00 504",
       "LWDD 00 506",     // High Byte laden
-      "BCD 182",          // Wenn das Carry-Flag gesetzt ist, gehe direkt zu Else.
+      "BCD 176",         // Adresse 170: Wenn das Carry-Flag gesetzt ist, gehe direkt zu Else.
       "SLL",             // Shift nach links
-      "BD 188",           // Else überspringen
+      "BD 182",          // Else überspringen
       "SLL",             // Else: Shift nach links
       "INC",             // Plus 1
-      "SWDD 00 506",     // Abspeichern
+      "SWDD 00 506",     // Adresse 180: Abspeichern
 
-      //Adresse 188
+      //Adresse 182
       "LWDD 00 502",
       "SLL",             // Das MSB von Faktor b ins Carry schreiben.
       "SWDD 00 502",     // Wider speichern
-      "BCD 198",          // Ist das Flag gesetzt: Faktor a Addieren.
-      "BD 204",            // If Teil überspringen
+      "BCD 192",         // Ist das Flag gesetzt: Faktor a Addieren.
+      "BD 206",          // Adresse 190: If Teil überspringen
       "LWDD 00 504",     // Faktor A zum Ergebnis addieren.
       "ADD 10",          //
       "SWDD 00 504",
-      "BCD 208",         // Überlauf bei Lower: 1 zu upper addieren.
-      "BD 212",          // If Teil überspringen
+      "BCD 202",         // Überlauf bei Lower: 1 zu upper addieren.
+      "BD 208",          // Adresse 200: If Teil überspringen
       "LWDD 00 506",
       "INC",             // 1 zu upper hinzufügen.
       "SWDD 00 506",
 
       //noadd:
       "LWDD 00 510",
-      "DEC",
+      "DEC",              //Adresse 210
       "SWDD 00 510",
       "BNZD 168",
 
       //vorzeichen
       "LWDD 00 512",     // Zahl aus Speicher 512 laden. Wenn 1, muss Resultat negiert werden.
       "SRL",             // LSB checken
-      "BCD 230",
+      "BCD 224",         //Adresse 220
       "END",
       "LWDD 00 504",     // Lower Zahl laden
       "NOT",
       "INC",
-      "SWDD 00 504",
+      "SWDD 00 504",     //Adresse 230
       "LWDD 00 506",
       "NOT",
       "INC",
       "SWDD 00 506",
       "END"
-
-
 
       ].join("\n")
         }
